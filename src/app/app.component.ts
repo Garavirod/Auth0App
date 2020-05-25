7655ff41575dec02717ccd2167a5efc1fb5e3d16
Garavirod <rodrigogarciaavila26@gmail.com>
2020-05-24 19:54:03 -0500

    Guradando el estado del usuario y su sesionactiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -9,4 +9,5 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   constructor(private auth: AuthService) {}
   title = 'AuthApp';
+
 }
